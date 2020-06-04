6337d7676505917e2fea765670586a8f0d22d6ab
Lucas Santos <lhs.santoss@gmail.com>
2020-06-04 19:09:46 -0300

    🔥 Removed unused class

--- src/classes/ThePracticalDev.ts
+++
@@ -1,5 +0,0 @@
-import { ThePracticalDevClient } from '../repositories/ThePracticalDevClient'
-
-export class ThePracticalDev {
-  constructor (private readonly client: ThePracticalDevClient) { }
-}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
-import { ThePracticalDev } from './classes/ThePracticalDev'
 import { ThePracticalDevClient } from './repositories/ThePracticalDevClient'
 
-export function Client (APIKey: string): ThePracticalDev {
+export function Client (APIKey: string): ThePracticalDevClient {
   const client = new ThePracticalDevClient(APIKey)
-  const instance = new ThePracticalDev(client)
-  return instance
+  return client
 }
